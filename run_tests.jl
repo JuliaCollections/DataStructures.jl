5b6fde360bf33557797ec339b5c5f6334c675088
Dahua Lin <lindahua@gmail.com>
2013-07-27 09:33:12 -0500

    use run_tests.jl in the place of test/test_all.jl

---
+++ run_tests.jl
@@ -0,0 +1,12 @@
+tests = ["deque", 
+		 "stack_and_queue", 
+		 "disjoint_set", 
+		 "binheap", 
+		 "mutable_binheap"]
+
+for t in tests
+	fp = joinpath("test", "test_$t.jl")
+	println("$fp ...")
+	include(fp)
+end
+
--- test/test_all.jl
+++
@@ -1,5 +0,0 @@
-include("test_deque.jl")
-include("test_stack_and_queue.jl")
-include("test_disjoint_set.jl")
-include("test_binheap.jl")
-include("test_mutable_binheap.jl")
