30cbb9a43fd6f0e4db179fa2e8511f6200125d28
StephenVavasis <vavasis@uwaterloo.ca>
2014-10-11 19:30:23 -0400

    more token operations, currently undefined

--- src/tokens.jl
+++ src/tokens.jl
@@ -1,6 +1,11 @@
 module Tokens
 
 abstract AbstractSemiToken
+import Base.getindex
+import Base.setindex!
+import Base.isless
+import Base.isequal
+import Base.colon
 
 immutable IntSemiToken <: AbstractSemiToken
     address::Int
@@ -15,7 +20,21 @@ end
 semi(i::Token) = i.semitoken
 container(i::Token) = i.container
 assemble(m, s::AbstractSemiToken) = Token(m,s)
+deref_key(i::Token) = error("undefined operation")
+deref_value(i::Token) = error("undefined operation")
+deref(i::Token) = error("undefined operation")
+status(i::Token) = error("undefined operation")
+getindex(m, i::AbstractSemiToken) = error("undefined operation")
+settindex!(m, i::AbstractSemiToken) = error("undefined operation")
+delete!(i::Token) = error("undefined operation")
+advance(i::Token) = error("undefined operation")
+regress(i::Token) = error("undefined operation")
+isless(i::Token, j::Token) = error("undefined operation")
+isequal(i::Token, j::Token) = error("undefined operation")
+colon(i::Token, j::Token) = error("undefined operation")
 
-export semi, container, assemble
+
+export semi, container, assemble, deref_key, deref_value
+export deref, status, delete!, advance, regress
 
 end
