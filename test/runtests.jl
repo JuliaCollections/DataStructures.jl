bcda551da314381b9be424d67c66106bf9ed4722
Yichao Yu <yyc1992@gmail.com>
2015-05-01 21:56:19 -0400

    Fix error on 0.4-dev, allow running tests without installing

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ tests = ["deque",
          "list"]
 
 for t in tests
-    fp = joinpath(Pkg.dir("DataStructures"), "test", "test_$t.jl")
+    fp = joinpath(dirname(@__FILE__), "test_$t.jl")
     println("$fp ...")
     include(fp)
 end
