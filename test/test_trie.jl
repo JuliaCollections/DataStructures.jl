803265ef7a08ddad227e695a8ec0e78a71cc085d
Gideon Dresdner <gideond@gmail.com>
2015-05-13 00:58:26 +0300

    wip, figuring out haskey

--- test/test_trie.jl
+++ test/test_trie.jl
@@ -4,36 +4,34 @@ using Base.Test
 t=Trie{Char,Int}()
 
 t["amy"]=56
-t["ann"]=15
-t["emma"]=30
-t["rob"]=27
-t["roger"]=52
+# t["ann"]=15
+# t["emma"]=30
+# t["rob"]=27
+# t["roger"]=52
 
-@test haskey(t, "roger")
-@test get(t,"rob",nothing) == 27
-@test sort(keys(t)) == ["amy", "ann", "emma", "rob", "roger"]
-@test t["rob"] == 27
-@test sort(keys_with_prefix(t,"ro")) == ["rob", "roger"]
+# @test haskey(t, "roger")
+# @test get(t,"rob",nothing) == 27
+# @test sort(keys(t)) == ["amy", "ann", "emma", "rob", "roger"]
+# @test t["rob"] == 27
+# @test sort(keys_with_prefix(t,"ro")) == ["rob", "roger"]
 
-@test true == false
-
-# constructors
-ks = ["amy", "ann", "emma", "rob", "roger"]
-vs = [56, 15, 30, 27, 52]
-kvs = collect(zip(ks, vs))
-@test typeof(Trie(ks, vs)) == Trie{Int}
-@test typeof(Trie(kvs)) == Trie{Int}
-@test typeof(Trie(Dict(kvs))) == Trie{Int}
-@test typeof(Trie(ks)) == Trie{Nothing}
+# # constructors
+# ks = ["amy", "ann", "emma", "rob", "roger"]
+# vs = [56, 15, 30, 27, 52]
+# kvs = collect(zip(ks, vs))
+# @test typeof(Trie(ks, vs)) == Trie{Int}
+# @test typeof(Trie(kvs)) == Trie{Int}
+# @test typeof(Trie(Dict(kvs))) == Trie{Int}
+# @test typeof(Trie(ks)) == Trie{Nothing}
 
 
-# path iterator
-t0 = t
-t1 = t0.children['r']
-t2 = t1.children['o']
-t3 = t2.children['b']
-@test collect(path(t, "b")) == [t0]
-@test collect(path(t, "rob")) == [t0, t1, t2, t3]
-@test collect(path(t, "robb")) == [t0, t1, t2, t3]
-@test collect(path(t, "ro")) == [t0, t1, t2]
-@test collect(path(t, "roa")) == [t0, t1, t2]
+# # path iterator
+# t0 = t
+# t1 = t0.children['r']
+# t2 = t1.children['o']
+# t3 = t2.children['b']
+# @test collect(path(t, "b")) == [t0]
+# @test collect(path(t, "rob")) == [t0, t1, t2, t3]
+# @test collect(path(t, "robb")) == [t0, t1, t2, t3]
+# @test collect(path(t, "ro")) == [t0, t1, t2]
+# @test collect(path(t, "roa")) == [t0, t1, t2]
