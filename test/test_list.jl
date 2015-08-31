a4f4f1cb7fbe0a527763e93ae9ecdc7736f16949
kshyatt <kshyatt@physics.ucsb.edu>
2015-08-30 23:22:46 -0700

    More tests for lists

--- test/test_list.jl
+++ test/test_list.jl
@@ -1,12 +1,22 @@
 using DataStructures
 using Base.Test
 
+l0 = nil(Char)
+@test length(l0) == 0
+@test sprint(show,l0) == "nil(Char)"
+
 l1 = nil()
 @test length(l1) == 0
+@test sprint(show,l1) == "nil()"
+@test typeof(list()) == typeof(l1)
+@test copy(l1) == l1
+@test map((x) -> x*2,l1) == l1
 
 l2 = cons(1, l1)
 @test length(l2) == 1
 @test head(l2) == 1
+@test sprint(show,l2) == "list(1)"
+@test cat(l2) == l2
 
 l3 = list(2, 3)
 @test is(typeof(l3), Cons{Int})
@@ -14,10 +24,14 @@ l3 = list(2, 3)
 @test head(l3) == 2
 @test head(tail(l3)) == 3
 @test collect(l3) == [2; 3]
+@test collect(copy(l3)) == [2; 3]
+@test sprint(show,l3) == "list(2, 3)"
 
 l4 = cat(l1, l2, l3)
 @test length(l4) == 3
 @test collect(l4) == [1; 2; 3]
+@test collect(copy(l4)) == [1; 2; 3]
+@test sprint(show,l4) == "list(1, 2, 3)"
 
 l5 = map((x) -> x*2, l4)
 @test typeof(l5) == Cons{Int}
@@ -43,3 +57,6 @@ l8 = cat(l5b, l6)
 l9 = cat(list(1, 2), list(3.0, 4.0))
 @test typeof(l9) == Cons{Real}
 @test collect(l9) == [1; 2; 3.0; 4.0]
+
+l10 = list(2, 4, 5.6, 10.5)
+@test collect(l10) == [2; 4; 5.6; 10.5]
