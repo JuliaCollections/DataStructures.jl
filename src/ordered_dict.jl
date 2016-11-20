32a9ddbba25a04ca907531507975fc66eb520804
Peter M. Haverty <phaverty@gene.com>
2016-11-20 10:44:43 -0800

    a few stray spaces in the OrderedDict type docstring

--- src/ordered_dict.jl
+++ src/ordered_dict.jl
@@ -15,7 +15,7 @@ end
 """
 OrderedDict
 
-`OrderedDict`s are  simply dictionaries  whose entries  have a  particular order.  The order
+`OrderedDict`s are simply dictionaries whose entries have a particular order.  The order
 refers to insertion order, which allows deterministic iteration over the dictionary or set.
 """
 type OrderedDict{K,V} <: Associative{K,V}
