4a9edb3aadda8d58f2eb97cb73b755fd4ddc917a
Tony Kelman <tony@kelman.net>
2017-01-07 14:26:53 -0500

    Revert "Remove HashDict"
    This reverts commit 1681e24c8813013d5d469a3cb5fa5ff65a0a3f3e.

--- src/dict_support.jl
+++ src/dict_support.jl
@@ -1,6 +1,6 @@
-# support functions
+# support function
 
-# These functions are defined in Base, but are not exported,
-# so they are redefined here.
+# This is defined in Base, but probably not meant for external consumption,
+# so it's redefined here.
 _tablesz(x::Integer) = x < 16 ? 16 : one(x)<<((sizeof(x)<<3)-leading_zeros(x-1))
-hashindex(key, sz) = (reinterpret(Int,(hash(key))) & (sz-1)) + 1
+
