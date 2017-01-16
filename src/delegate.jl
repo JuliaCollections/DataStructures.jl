1373a7b2cd6e9145165124271f0977bf0a6bfc76
Kevin Squire <kevin@secondspectrum.com>
2017-01-15 22:05:59 -0800

    Fix delegation for Dict functions returning the underlying Dict
    * When called on an Associative container, delete!, empty!, sizehint!
      and setindex! all return the container
    * The `@delegate` macro is not aware of this, and instead returned
      the wrapped object.
    * Fixed by creating a second macro, `@delegate_return_parent`,
      which works like `@delegate`, but returns the parent container.

--- src/delegate.jl
+++ src/delegate.jl
@@ -4,12 +4,28 @@ macro delegate(source, targets)
     fieldname = source.args[2].args[1]
     funcnames = targets.args
     n = length(funcnames)
-    fdefs = Array(Any, n)
+    fdefs = Vector{Any}(n)
     for i in 1:n
         funcname = esc(funcnames[i])
         fdefs[i] = quote
                      ($funcname)(a::($typename), args...) =
                        ($funcname)(a.$fieldname, args...)
+                   end
+    end
+    return Expr(:block, fdefs...)
+end
+
+macro delegate_return_parent(source, targets)
+    typename = esc(source.args[1])
+    fieldname = source.args[2].args[1]
+    funcnames = targets.args
+    n = length(funcnames)
+    fdefs = Vector{Any}(n)
+    for i in 1:n
+        funcname = esc(funcnames[i])
+        fdefs[i] = quote
+                     ($funcname)(a::($typename), args...) =
+                       (($funcname)(a.$fieldname, args...); a)
                    end
     end
     return Expr(:block, fdefs...)
