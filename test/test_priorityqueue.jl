3c2206cac1c8cd3038bb935f469065e4d05b6272
Kevin Squire <kevin@secondspectrum.com>
2017-08-15 22:48:12 -0700

    Update PriorityQueue tests for new code
    * Added parameterized constructor tests
    * Added tests for enqueueing pairs

--- test/test_priorityqueue.jl
+++ test/test_priorityqueue.jl
@@ -35,52 +35,76 @@ end
     priorities = Dict(zip(1:n, r))
 
     # building from a dict
-    pq = PriorityQueue(priorities)
-    test_issorted!(pq, priorities)
+    pq1 = PriorityQueue(priorities)
+    test_issorted!(pq1, priorities)
 
-    pq = PriorityQueue(priorities)
-    test_isrequested!(pq, 1:n)
+    pq2 = PriorityQueue(priorities)
+    test_isrequested!(pq2, 1:n)
 
     # building from two lists (deprecated)
     ks, vs = 1:n, rand(1:pmax, n)
     println("The following warning is expected:")
-    pq = PriorityQueue(ks,vs)
+    pq3 = PriorityQueue(ks,vs)
 
-    # building from two lists - error throw
+    # building from two lists of different sizes - throws an error
     ks, vs = 1:n+1, rand(1:pmax, n)
     println("The following warning is expected:")
     @test_throws ArgumentError PriorityQueue(ks, vs)
 
     # building from Dict
     priorities = Dict(zip(ks, vs))
-    pq = PriorityQueue(priorities)
-    test_issorted!(pq, priorities)
+    pq4 = PriorityQueue(priorities)
+    test_issorted!(pq4, priorities)
 
     # parameterized constructor tests
-    pq = PriorityQueue{Int,Int}()
+    pq5 = PriorityQueue{Int,Int}()
     for (k,v) in priorities
-        pq[k] = v
+        pq5[k] = v
     end
-    test_issorted!(pq, priorities)
+    test_issorted!(pq5, priorities)
 
     # parameterized constructor tests
-    pq = PriorityQueue{Int,Int}(Reverse, priorities)
-    test_issorted!(pq, priorities, true)
+    pq6 = PriorityQueue{Int,Int}(priorities)
+    test_issorted!(pq6, priorities)
 
     # parameterized constructor tests
-    pq = PriorityQueue{Int,Int}(Reverse)
+    pq7 = PriorityQueue{Int,Int}(Reverse, priorities)
+    test_issorted!(pq7, priorities, true)
+
+    # parameterized constructor tests
+    pq8 = PriorityQueue{Int,Int}(Reverse)
     for (k,v) in priorities
-        pq[k] = v
+        pq8[k] = v
     end
-    test_issorted!(pq, priorities, true)
+    test_issorted!(pq8, priorities, true)
+
+    # construction from pairs
+    pq9 = PriorityQueue('a'=>1, 'b'=>2)
+    @test peek(pq9) == ('a'=>1)
+    
+    # duplicate key => ArgumentError
+    @test_throws ArgumentError PriorityQueue('a'=>1, 'a'=>2)
+
+    # No pair/tuple => ArguentError
+    @test_throws ArgumentError PriorityQueue(['a'])
+
+    # Silly test
+    @test_throws ArgumentError PriorityQueue(Reverse, Reverse)
 
     # peek
-    pq = PriorityQueue(priorities)
+    pq10 = PriorityQueue(priorities)
     lowpri = findmin(vs)
-    @test peek(pq)[2] == pq[ks[lowpri[2]]]
+    @test peek(pq10)[2] == pq10[ks[lowpri[2]]]
 end
 
-@testset "EnqueueDequeue" begin
+@testset "PriorityQueueMethods" begin
+    pq1 = PriorityQueue('a'=>1, 'b'=>2)
+    @test peek(pq1) == ('a'=>1)
+    @test get(pq1, 'a', 0) == 1
+    @test get(pq1, 'c', 0) == 0
+    @test dequeue!(pq1) == 'a'
+    @test dequeue!(pq1) == 'b'
+
     #enqueue error throw
     pmax = 1000
     n = 10000
@@ -91,13 +115,28 @@ end
     pq = PriorityQueue(zip(ks, vs))
     @test_throws ArgumentError enqueue!(pq, 1, 10)
 
-    # enqueing via enqueue!
+    # Iteration
+
+    pq = PriorityQueue(priorities)
+    pq2 = PriorityQueue()
+    for kv in pq
+        enqueue!(pq2, kv)
+    end
+    @test pq == pq2
+
+    # enqueing pairs via enqueue!
+    pq = PriorityQueue()
+    for kv in priorities
+        enqueue!(pq, kv)
+    end
+    test_issorted!(pq, priorities)
+    
+    # enqueing values via enqueue!
     pq = PriorityQueue()
     for (k, v) in priorities
         enqueue!(pq, k, v)
     end
     test_issorted!(pq, priorities)
-
 
     # enqueing via assign
     pq = PriorityQueue()
@@ -123,12 +162,8 @@ end
 
     # dequeuing
     pq = PriorityQueue(priorities)
-    try
-        dequeue!(pq, 0)
-        error("should have resulted in KeyError")
-    catch ex
-        @test isa(ex, KeyError)
-    end
+    @test_throws KeyError dequeue!(pq, 0)
+
     @test 10 == dequeue!(pq, 10)
     while !isempty(pq)
         @test 10 != dequeue!(pq)
@@ -137,8 +172,8 @@ end
     priorities2 = Dict(zip('a':'e', 5:-1:1))
     pq = PriorityQueue(priorities2)
     @test_throws KeyError dequeue_pair!(pq, 'g')
-    @test dequeue_pair!(pq) == Pair('e', 1)
-    @test dequeue_pair!(pq, 'b') == Pair('b', 4)
+    @test dequeue_pair!(pq) == ('e'=> 1)
+    @test dequeue_pair!(pq, 'b') == ('b'=>4)
     @test length(pq) == 3
 end
 
