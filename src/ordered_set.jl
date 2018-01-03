bb6b722eddbf7c324226a47a6ef89d8737f70b3e
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-01-03 09:50:52 +0100

    Base.join_eltype was removed in JuliaLang/julia/#23528

--- src/ordered_set.jl
+++ src/ordered_set.jl
@@ -52,7 +52,7 @@ pop!(s::OrderedSet) = pop!(s.dict)[1]
 
 union(s::OrderedSet) = copy(s)
 function union(s::OrderedSet, sets...)
-    u = OrderedSet{Base.join_eltype(s, sets...)}()
+    u = OrderedSet{Base.promote_eltype(s, sets...)}()
     union!(u,s)
     for t in sets
         union!(u,t)
