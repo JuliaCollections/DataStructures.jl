c1dd8b2211dc3e23d0410a657146b7322895d564
autodocs <autodocs>
2018-09-11 09:08:59 +0000

    build based on bdd7aa0

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.12",
   "release-0.11",
   "release-0.9",
   "release-0.8",
   "release-0.7",
+  "v0.12.0",
   "v0.11.1",
   "v0.9.0",
   "V0.8.4",
