3f5c280dbed8752d4fa277319f8ad299f9b1f027
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-24 16:47:20 -0700

    Define methods for specific arities in delegate macros

--- src/delegate.jl
+++ src/delegate.jl
@@ -9,6 +9,18 @@ function unquote(e::QuoteNode)
     return e.value
 end
 
+const _arities = Dict(
+    :get => 3,
+    :get! => 3,
+)
+
+_argsig(funcname) =
+    if haskey(_arities, funcname)
+        :(args::Vararg{<:Any, $(_arities[funcname] - 1)})
+    else
+        :(args::Vararg)
+    end
+
 macro delegate(source, targets)
     typename = esc(source.args[1])
     fieldname = unquote(source.args[2])
@@ -18,7 +30,7 @@ macro delegate(source, targets)
     for i in 1:n
         funcname = esc(funcnames[i])
         fdefs[i] = quote
-                     ($funcname)(a::($typename), args...) =
+                     ($funcname)(a::($typename), $(_argsig(funcnames[i]))) =
                        ($funcname)(a.$fieldname, args...)
                    end
     end
@@ -34,7 +46,7 @@ macro delegate_return_parent(source, targets)
     for i in 1:n
         funcname = esc(funcnames[i])
         fdefs[i] = quote
-                     ($funcname)(a::($typename), args...) =
+                     ($funcname)(a::($typename), $(_argsig(funcnames[i]))) =
                        (($funcname)(a.$fieldname, args...); a)
                    end
     end
