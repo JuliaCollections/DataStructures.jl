f71326ebf9aa7fb1a25113d4fd6c983c3338d191
Alek Westover <alek.westover@gmail.com>
2020-04-04 00:58:16 -0400

    remove code fragment

--- src/fenwick.jl
+++ src/fenwick.jl
@@ -84,8 +84,6 @@ julia> prefixsum(f, 3)
 function prefixsum(ft::FenwickTree{T}, ind::Integer) where T
     sum = zero(T)
     ind < 1 && return sum
-        return 0
-    end
     i = ind
     n = ft.n
     @boundscheck 1 <= i <= n || throw(ArgumentError("$i should be in between 1 and $n"))
