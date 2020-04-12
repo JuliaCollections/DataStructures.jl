6058f92877b34b9fdb9eab047c51e21c623cc6a1
Lyndon White <oxinabox@ucc.asn.au>
2020-04-12 15:11:17 +0100

    more export of old find_root to deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -2,4 +2,5 @@
 @deprecate back(x) last(x)
 @deprecate top(x) first(x)
 #@deprecate find_root find_root! # 2020-03-31 - deprecate in v0.18, or when Julia 1.5 is released.
+export find_root
 const find_root = find_root!
