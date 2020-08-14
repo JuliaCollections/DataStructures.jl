c6d57cf3b98f9c59d9ccd5d274ac4d0556983637
eulerkochy <kc99.kol@gmail.com>
2020-08-14 20:08:23 +0530

    remove include statement from robin_dict.jl

--- src/DataStructures.jl
+++ src/DataStructures.jl
@@ -12,7 +12,8 @@ module DataStructures
                  isless, union, intersect, symdiff, setdiff, issubset,
                  searchsortedfirst, searchsortedlast, in,
                  eachindex, keytype, valtype, minimum, maximum, size,
-                 zero, checkbounds
+                 zero, checkbounds, filter!, isbitstype, isbitsunion, 
+                 isiterable, dict_with_eltype, KeySet, Callable, _tablesz
 
 
     using OrderedCollections
