43f672d76c0c71a07db9f8777eca1c93864e1dea
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2022-03-13 13:01:16 +0700

    Update test_segment_tree.jl

---
+++ test/test_segment_tree.jl
@@ -0,0 +1,13 @@
+@testset "segment_tree" begin
+    @testset "Add" begin
+        X1 = segment_tree(100,Base.+)
+        
+        
+    end
+
+
+    @testset "Large_randomized_trial" begin
+        
+    end
+
+end
