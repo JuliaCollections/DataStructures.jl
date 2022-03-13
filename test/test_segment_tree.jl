1f995006d9d935ea3413e5a02777246e9dd58fcb
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2022-03-13 13:14:48 +0700

    Update test_segment_tree.jl

--- test/test_segment_tree.jl
+++ test/test_segment_tree.jl
@@ -1,8 +1,15 @@
 @testset "segment_tree" begin
     @testset "Add" begin
-        X1 = segment_tree(100,Base.+)
-        
-        
+        X1 = segment_tree(100,Base.:+)
+        a = zeros(Int64, 100)
+        change_range!(X1, 3,37,53)
+        change_range!(X1, 9,23,45)
+        change_range!(X1, 5,2,21)
+        a[37:53] .= 3
+        a[23:45] .= 9
+        a[2:21]  .= 5
+        @test sum(a[23:99]) == get_range(X1, 23,99)
+
     end
 
 
