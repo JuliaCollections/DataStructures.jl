d4cd0760757e120665feb07289588d5bc2bc924e
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2022-04-11 10:46:53 +0700

    Finishing lots of things.
    It takes a lot to set up the projects for this.

--- src/Segment_tree.jl
+++ src/Segment_tree.jl
@@ -1,6 +1,14 @@
+#=
+Author: Alice Roselia.
+It should be made available as part of the (MIT-licensed) Datastructures.jl package.
+Feel free to use or extend this code.
+=#
+
+
+
 abstract type Abstractsegmenttreenode{Dtype, Op, iterated_op} end
 abstract type Abstractsegmenttree{node_type} end
-
+Standard_Field = Union{Real, Complex}
 #=
 Segment trees have the following traits.
 1) can_add_range: either Val{True} or Val{False}.
@@ -12,6 +20,10 @@ Dispatching will have "valid". Dispatching for invalid would be error.
 can_add_range(::Abstractsegmenttree)= false;
 can_change_range(::Abstractsegmenttree) = true;
 is_functional(::Abstractsegmenttree) = false;
+#Convenience so you don't have to use arguments every time.
+get_dtype(::Abstractsegmenttreenode{Dtype,Op,iterated_op}) where {Dtype, Op, iterated_op} = Dtype
+get_op(::Abstractsegmenttreenode{Dtype,Op,iterated_op}) where {Dtype, Op, iterated_op} = Op
+get_iterated_op(::Abstractsegmenttreenode{Dtype,Op,iterated_op}) where {Dtype, Op, iterated_op} = iterated_op
 
 
 function repeat_op(base,time::Integer, op::Function)
@@ -47,7 +59,6 @@ end
 mutable struct Segment_tree{node_type<:Abstractsegmenttreenode}<:Abstractsegmenttree{node_type}
     size::UInt64
     head::node_type
-    
 end
 function Segment_tree(size::Number,Dtype::Type,op::Function, iterated_op::Function)
     newsize = convert(UInt64,size)
@@ -55,13 +66,13 @@ function Segment_tree(size::Number,Dtype::Type,op::Function, iterated_op::Functi
     return Segment_tree{node_type}(newsize, node_type())
 end
 
-Addition_supported = Union{Real, Complex}
-
+sizeof(X::Abstractsegmenttree) = X.size
+get_head(X::Abstractsegmenttree) = X.head
 Segment_tree(size::Number, T::Type, op::Function) = Segment_tree(size, T, op, (x,y)->repeat_op(x,y,op))
-Segment_tree(size::Number,::Type{T}, ::typeof(+)) where {T<:Addition_supported} = Segment_tree(size,T,+,*)
-Segment_tree(size::Number,::Type{Array{T,N}}, ::typeof(+)) where {T<:Addition_supported,N} = Segment_tree(size,Array{T,N},+,*)
-Segment_tree(size::Number,::Type{T}, ::typeof(*)) where {T<:Addition_supported} = Segment_tree(size,T,*,^)
-Segment_tree(size::Number,::Type{Array{T,N}}, ::typeof(*)) where {T<:Addition_supported,N} = Segment_tree(size,Array{T,N},*,^)
+Segment_tree(size::Number,::Type{T}, ::typeof(+)) where {T<:Standard_Field} = Segment_tree(size,T,+,*)
+Segment_tree(size::Number,::Type{Array{T,N}}, ::typeof(+)) where {T<:Standard_Field,N} = Segment_tree(size,Array{T,N},+,*)
+Segment_tree(size::Number,::Type{T}, ::typeof(*)) where {T<:Standard_Field} = Segment_tree(size,T,*,^)
+Segment_tree(size::Number,::Type{Array{T,N}}, ::typeof(*)) where {T<:Standard_Field,N} = Segment_tree(size,Array{T,N},*,^)
 
 struct functional_segment_tree{node_type<:Abstractsegmenttreenode}<:Abstractsegmenttree{node_type}
     size::UInt64
@@ -71,17 +82,18 @@ is_functional(functional_segment_tree) = true
 
 
 mutable struct Segment_tree_node{Dtype, Op, iterated_op, identity}<:Abstractsegmenttreenode{Dtype,Op,iterated_op}
-    child_nodes::Union{NTuple{2,Segment_tree_node{Dtype, Op, iterated_op}},Nothing}
+    child_nodes::Union{NTuple{2,Segment_tree_node{Dtype, Op, iterated_op, identity}},Nothing}
     #Either both children are valid or none is valid. 
     value::Dtype
     density::Dtype
+    #Implicitly have information about where it represents.
     function Segment_tree_node()
         return new(nothing)
     end
 end
 
 mutable struct Segment_tree_node_without_identity{Dtype,Op,iterated_op}<:Abstractsegmenttreenode{Dtype,Op,iterated_op}
-    child_nodes::Union{NTuple{2,Segment_tree_node{Dtype, Op, iterated_op}},Nothing}
+    child_nodes::Union{NTuple{2,Segment_tree_node_without_identity{Dtype, Op, iterated_op}},Nothing}
     #Either both children are valid or none is valid. 
     #This variant will throw an error instead of returning an identity.
     #This is not very recommended, as complexity is required to bypass this.
@@ -94,6 +106,69 @@ end
 
 
 
-function Segment_tree_node()
+function Segment_tree_node(Dtype::Type{T}, Op::Function, iterated_op::Function, identity::T)
+    return Segment_tree_node{Dtype,Op,iterated_op,identity}()
+end
+
+get_identity(X,op::Function) = get_identity(typeof(X),op)
+get_identity(::Type,::Function) = nothing
+get_identity(::Type{X},typeof(+)) where {X<:Standard_Field} = zero(X)
+get_identity(::Type{X},typeof(*)) where {X<:Standard_Field} = one(X)
+function Segment_tree_node(Dtype::Type, Op::Function, iterated_op::Function)
+    if get_identity(Dtype,Op) isa Nothing
+        return Segment_tree_node_without_identity{Dtype,Op,iterated_op}()
+    else
+        return Segment_tree_node{Dtype,Op,iterated_op,get_identity(Dtype,Op)}()
+    end
+end
+
+Standard_Segment_tree_node = Union{Segment_tree_node,Segment_tree_node_without_identity} #Standard meaning not every Segment_tree_node has this.
+
+get_left_child(X::Standard_Segment_tree_node) = X.child_nodes[1]
+get_right_child(X::Standard_Segment_tree_node) = X.child_nodes[2]
+
+
+
+function get_range(X::Abstractsegmenttree, low, high)
+    # get the reduce(Op, X[i] for all low<=i<=high)
+    return get_range(get_head(X),low,high, 1, sizeof(X))
+end
+
+
+
+
+#Implement get_op, get_iterated_op, etc...
+
+function get_range(X::Standard_Segment_tree_node, Query_low, Query_high, Current_low, Current_high)
     
+    #while the tree is still not at splitting point
+        #if on low 
+            #Go down low path.
+        #else
+            #Go down high path.
+        #end
+    #end
+    #Split into two functions, ones where you only care about prefix, and the other where you only care about suffix.
+    while true
+        Current_mid = div(low+high,2)
+        if Query_high <= Current_mid
+            Current_high = Current_mid
+            X = get_left_child(X)
+        else if Query_low > Current_mid
+            Current_low = Current_mid
+            X = get_right_child(X)
+        else
+            return get_op(X)(get_left_range(get_left_child(X), Query_low, Current_low),get_right_range(get_right_child(X,Query_high, Current_high)))
+            #Working in progress.
+        end
+
+    end
+end
+
+function get_left_range(X::Standard_Segment_tree_node, Query_low, Current_low)
+
+end
+
+function get_right_range(X::Standard_Segment_tree_node, Query_high, Current_high)
+
 end
--- test/test_segment_tree.jl
+++ test/test_segment_tree.jl
@@ -1,3 +1,42 @@
+#=
+Author: Alice Roselia.
+It should be made available as part of the (MIT-licensed) Datastructures.jl package.
+Feel free to use or extend this code.
+=#
+
+
+
+
+
+
+
+#=
+Putting my Quarternion to test.
+
+Quarternion also has associative property, so it should work.
+
+This is NOT a complete Quarternion implementation. It is only used here for testing purpose.
+This code belongs to me (it's my early julia codes) but you could've written this code too. 
+Just a literal implementation of Quarternion based on its definition, no optimization or other methods. 
+=#
+
+
+
+struct Quarternion{T<:Real}
+    real::T
+    i::T
+    j::T
+    k::T
+end
+
+#Base.:+(a::Quarternion, b::Quarternion) = Quarternion(a.real+b.real, a.i+b.i, a.j+b.j, a.k+b.k) Not used in the test.
+Base.:*(a::Quarternion, b::Quarternion) =
+Quarternion(a.real*b.real - a.i*b.i - a.j*b.j - a.k*b.k,
+            a.real*b.i + b.real*a.i + a.j*b.k - a.k*b.j,
+            a.real*b.j + b.real*a.j + a.k*b.i - a.i*b.k,
+            a.real*b.k + b.real*a.k + a.i*b.j - a.j*b.i,
+)
+
 @testset "segment_tree" begin
     @testset "Add" begin
         X1 = segment_tree(100,UInt64,Base.:+)
@@ -15,6 +54,8 @@
     end
 
     @testset "Large_randomized_trial" begin
+
+        #Don't worry about the overflow. This is unsigned integer.
         X1 = segment_tree(10000,UInt64, Base.:+)
         X2 = zeros(UInt64, 10000)
         for i in 1:10000
@@ -29,4 +70,39 @@
         end
     end
 
+    @testset "Xor_trial" begin
+        X1 = segment_tree(10000,UInt64, xor)
+        X2 = zeros(UInt64, 10000)
+        for i in 1:10000
+            a = rand(1:10000)
+            b = rand(a:10000)
+            c = rand(UInt64)
+            change_range!(X1,c,a,b)
+            X2[a:b] = c
+            d = rand(1:10000)
+            e = rand(d:10000)
+            @test reduce(xor,X2[d:e]) == get_range(X1,d,e)
+        end
+    end
+    
+    @testset "3x3_matrix_multiplication" begin
+        #Float/etc should work fine as well. Just don't want to deal with precision issues.
+        X1 = segment_tree(1000,Array{UInt64,2},*)
+        identity_matrix = zeros(UInt64,(3,3))
+        identity_matrix[1,1] = identity_matrix[2,2] = identity_matrix[3,3] = 1
+        #Vector of vector may not be the most efficient, but it should work without problem.
+        X2 = [identity_matrix for i in 1:1000]
+        #Viewing without copying should be fine, as we won't mutate the arrays.
+        #Static arrays recommended for serious uses of this.
+        for i in 1:1000
+            a = rand(1:1000)
+            b = rand(a:1000)
+            c = rand(UInt64,(3,3))
+            change_range!(X1,c,a,b)
+            X2[a:b] = c
+            d = rand(1:1000)
+            e = rand(d:1000)
+            @test reduce(*,X2[d:e]) == get_range(X1,d,e)
+        end
+    end
 end
