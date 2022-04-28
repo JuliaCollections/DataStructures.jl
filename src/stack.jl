1ccfc3e13eca0e98effb2ba8dad7daea28c54af0
lfenzo <lfenzo@protonmail.com>
2022-04-28 15:59:05 -0300

    docs: updated stack documentation page and docstrings

--- src/stack.jl
+++ src/stack.jl
@@ -1,27 +1,19 @@
 """
-    Stack{T}([blksize::Integer=1024])
+    Stack{T}() where {T}
+    Stack{T}(blksize::Integer) where {T}
 
-Create a `Stack` object containing elements of type `T` for Last In First Out (LIFO) access.
+Create a `Stack` object containing elements of type `T` for Last-In-First-Out (LIFO) access.
+
+# Parameters
+`blksize::Integer` Unrolled linked-list bleck size (in bytes). Defualt = 1024.
 
 # Examples
 ```jldoctest
-julia> s = Stack{Int}() # create a stack
+julia> s_int = Stack{Int64}() # create a stack with int elements
 Stack{Int64}(Deque [Int64[]])
 
-julia> push!(s, 1) # push back a item
-Stack{Int64}(Deque [[1]])
-
-julia> x = top(s) # get a first item
-1
-
-julia> length(s)
-1
-
-julia> x = pop!(s) # get and remove a first item
-1
-
-julia> length(s)
-0
+julia> s_float = Stack{Float64}() # create a stack with float elements
+Stack{Float64}(Deque [Float64[]])
 ```
 """
 mutable struct Stack{T}
@@ -31,29 +23,69 @@ end
 Stack{T}() where {T} = Stack(Deque{T}())
 Stack{T}(blksize::Integer) where {T} = Stack(Deque{T}(blksize))
 
+"""
+    isempty(s::Stack)
+
+Check if stack `s` is empty.
+"""
 Base.isempty(s::Stack) = isempty(s.store)
+
+
+"""
+    isempty(s::Stack)
+
+Return the number of elements in stack `s`.
+"""
 Base.length(s::Stack) = length(s.store)
-Base.eltype(::Type{Stack{T}}) where T = T
+
+
+"""
+    eltype(::Type{Stack{T}}) where {T}
+
+Return the type of the elements in the stack.
+"""
+Base.eltype(::Type{Stack{T}}) where {T}
 
 """
     first(s::Stack)
 
-Get the top item from the stack. Sometimes called peek.
+Get the top item from stack `s`. Also know as "peak".
 """
 Base.first(s::Stack) = last(s.store)
+
 Base.last(s::Stack) = first(s.store)
 
+"""
+    push!(s::Stack, x)
+
+Insert new element `x` in top of stack `s`.
+"""
 function Base.push!(s::Stack, x)
     push!(s.store, x)
     return s
 end
 
+"""
+   pop!(s::Stack) 
+
+Remove and return the top element from stack `s`.
+"""
 Base.pop!(s::Stack) = pop!(s.store)
 
+"""
+    empty!(s::Stack)
+
+Remove all elements from stack `s`.
+"""
 Base.empty!(s::Stack) = (empty!(s.store); s)
 
 Base.iterate(st::Stack, s...) = iterate(Iterators.reverse(st.store), s...)
 
 Iterators.reverse(s::Stack{T}) where {T} = DequeIterator{T}(s.store)
 
+"""
+    ==(x::Stack, y::Stack)
+
+Check if stacks `x` and `y` are equal in term of their contents.
+"""
 Base.:(==)(x::Stack, y::Stack) = x.store == y.store
