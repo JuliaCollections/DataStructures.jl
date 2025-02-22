bf66dd1113bfb6047519ae6324a980b5e55ef55b
Matthew Sotoudeh <matthew@masot.net>
2025-02-22 21:45:15 +0000

    Fix splay tree amortized time, deletions
    Fixes two bugs in the splay tree implementation:
    
        1. Not splaying on failed searches or redundant insertions can break
           the O(log n) amortized time guarantee.
    
           Fix: Always splay at the end of search_node!
    
        2. Deletion did not check that the node returned from search_node!
           had the desired key, so trying to delete keys not in the splay
           tree could cause it to accidentally delete unrelated keys.
    
           Fix: Check node.data for the return value of search_node!, as is
           done already for other operations.
    
    I've also added a regression test for bug (2).

--- src/DataStructures.jl
+++ src/DataStructures.jl
@@ -51,7 +51,7 @@ module DataStructures
     export DiBitVector
 
     export RBTree, search_node, minimum_node
-    export SplayTree, maximum_node
+    export SplayTree, search_node!, maximum_node
     export AVLTree, sorted_rank
     export SplayTree, maximum_node
 
