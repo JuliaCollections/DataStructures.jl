dd506395af641ddd02ad3c07fea5c6bb6ab31417
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-30 21:37:18 -0400

    Update src/dict_support.jl

--- src/dict_support.jl
+++ src/dict_support.jl
@@ -1,6 +1,6 @@
 # support functions
 
 function not_iterator_of_pairs(kv::T) where T
-    return Base.isiterable(kv) &&
+    return Base.isiterable(T) &&
            any(x->!isa(x, Union{Tuple,Pair}), kv)
 end
