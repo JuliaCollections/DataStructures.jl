f1db618f30d88ef9459516845e603db47ba6e917
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-04-30 21:42:48 -0400

    Simpler logic and fast path for known concrete-ish eltypes

--- src/dict_support.jl
+++ src/dict_support.jl
@@ -1,6 +1,16 @@
 # support functions
 
 function not_iterator_of_pairs(kv::T) where T
-    return !(Base.isiterable(T)) || # if the object is not iterable, return true, else check the eltype of the iteration
-           any(x->!isa(x, Union{Tuple,Pair}), kv)
+    # if the object is not iterable, return true, else check the eltype of the iteration
+    Base.isiterable(T) && return true 
+    # else, check if we can check `eltype`:
+    if Base.IteratorEltype(kv) isa Base.HasEltype
+        typ = eltype(kv)
+        if !(typ == Any)
+            return !(typ <: Union{<: Tuple, <: Pair})
+        end
+    end
+    # we can't check eltype, or eltype is not useful, 
+    # so brute force it.
+    return any(x->!isa(x, Union{Tuple,Pair}), kv)
 end
